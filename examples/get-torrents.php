6d8baf215d8d2877a9615a602f6d5633bd4ab9b9
PBX_g33k <development@oguzhanuysal.eu>
2018-03-10 13:23:44 +0100

    Updated test and fixed hostconfig

--- examples/get-torrents.php
+++ examples/get-torrents.php
@@ -11,13 +11,19 @@ $utorrentClient = new \Pbxg33k\UtorrentClient\UtorrentClient(
     $host['path']
 );
 
+
+$start = microtime(true);
+
 $utorrentClient->setCache(new \Cache\Adapter\Void\VoidCachePool());
 
 $torrents = $utorrentClient->getTorrents();
 
-print("Retrieved {$torrents->getTorrents()->count()} torrents\r\n");
+$getTime = microtime(true);
 
 /** @var \Pbxg33k\UtorrentClient\Model\Torrent $torrent */
 foreach($torrents->getTorrents() as $torrent) {
     print("Hash: {$torrent->getHash()} | Size: {$torrent->getSize()} | Name: {$torrent->getName()}\r\n");
 }
+
+$end = microtime(true);
+print("Retrieved {$torrents->getTorrents()->count()} torrents\r\n");
